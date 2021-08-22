f2c4e871c6733501d2958401cb4ddd894f2e4dbb
MD Fazal Mustafa <fazal@MDs-MacBook-Air.local>
2021-08-22 11:55:23 +0530

    Testing git in mac

--- basic.py
+++ basic.py
@@ -17,8 +17,8 @@ canny = cv.Canny(blur, 125, 175)
 cv.imshow('Canny Edges', canny)
 
 #Dilating the image
-dilated = cv.dilate(canny, (7,7), iterations=3)
-cv.imshow('Dilated',dilated)
+#dilated = cv.dilate(canny, (7,7), iterations=3)
+#cv.imshow('Dilated',dilated)
 
 #Eroding
 eroded = cv.erode(dilated, (7,7), iterations = 3)
